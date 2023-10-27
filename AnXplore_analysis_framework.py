ccf6ae958195168ec22e67e588385964b81428c0
UgoPelissier <upelissier@gmail.com>
2023-10-27 11:37:17 +0200

    🚀

--- AnXplore_analysis_framework.py
+++ AnXplore_analysis_framework.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     T_cardiac_cycle = 3
 
-    # decompress_h5(data_dir, filename)
+    decompress_h5(data_dir, filename)
 
     os.makedirs(osp.join(data_dir, "csv"), exist_ok=True)
     indicators = []
@@ -30,9 +30,19 @@ if __name__ == '__main__':
             indicators.append(compute_indicators(vtu_dir, time_step, vessel_in_out_origin, vessel_in_out_plane, orifice_origin, orifice_plane))
             bar()
 
+    # Compute min, max and mean over the cardiac cycle
+    indicators = np.array(indicators)
+    min = np.min(indicators, axis=0)
+    max = np.max(indicators, axis=0)
+    mean = np.mean(indicators, axis=0)
+    std = np.std(indicators, axis=0)
+    indicators = np.concatenate((indicators, min.reshape((1,-1)), max.reshape((1,-1)), mean.reshape((1,-1)), std.reshape((1,-1))))
+
     np.savetxt(
         fname=osp.join(data_dir, "csv", f"{filename[:-4]}_indicators.csv"),
         X=np.array(indicators),
         fmt="%.5f",
         header="min_wss max_wss min_wss_aneurysm max_wss_aneurysm min_wss_vessels max_wss_vessels mean_wss std_wss mean_wss_aneurysm std_wss_aneurysm mean_wss_vessels std_wss_vessels mean_osi_aneurysm max_osi_aneurysm, KER, VDR, LSA, HSA, SCI, ICI",
         comments='')
+    
+    os.system(f"rm -rf {vtu_dir}")
