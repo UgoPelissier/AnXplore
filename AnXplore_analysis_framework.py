5a4d1a424abce15a50833e07cf7dab7bb8427d80
UgoPelissier <upelissier@gmail.com>
2023-10-31 20:23:31 +0100

    🚀

--- AnXplore_analysis_framework.py
+++ AnXplore_analysis_framework.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import os.path as osp
 import numpy as np
@@ -9,9 +10,7 @@ from utils.indicators import compute_indicators
 if __name__ == '__main__':
 
     data_dir = "data"
-    filename = "AnXplore178_FSI.xdmf"
-
-    xdmf_file = XDMF_Wrapper(osp.join(data_dir, filename))    
+    cases = ['fsi']
 
     vessel_in_out_origin = [0.0, 0.001, 0.0]
     vessel_in_out_plane = [0.0, 1.0, 0.0]
@@ -21,29 +20,36 @@ if __name__ == '__main__':
 
     T_cardiac_cycle = 20
 
-    os.makedirs(osp.join(data_dir, "csv"), exist_ok=True)
-    indicators = []
+    for case in cases:
+        filenames = glob.glob(osp.join(data_dir, case, "*.xdmf"))
 
-    # Compute indicators over a cardiac cycle
-    with alive_bar(T_cardiac_cycle, title="Computing indicators over a cardiac cycle...") as bar:
-        time_steps = xdmf_file.get_time_steps()
-        for t in time_steps[-T_cardiac_cycle:]:
-            xdmf_file.update_time_step(t)
-            indicators.append(compute_indicators(xdmf_file, vessel_in_out_origin, vessel_in_out_plane, orifice_origin, orifice_plane))
-            bar()
-    print("Done.")
+        with alive_bar(T_cardiac_cycle, title=f"Computing indicators for {case} case") as bar:
+            for filename in filenames:
+                xdmf_file = XDMF_Wrapper(osp.join(filename))    
 
-    # Compute min, max and mean over the cardiac cycle
-    indicators = np.array(indicators)
-    min = np.min(indicators, axis=0)
-    max = np.max(indicators, axis=0)
-    mean = np.mean(indicators, axis=0)
-    std = np.std(indicators, axis=0)
-    indicators = np.concatenate((indicators, min.reshape((1,-1)), max.reshape((1,-1)), mean.reshape((1,-1)), std.reshape((1,-1))))
+                os.makedirs(osp.join(data_dir, "csv"), exist_ok=True)
+                indicators = []
 
-    np.savetxt(
-        fname=osp.join(data_dir, "csv", f"{filename[:-5]}_indicators.csv"),
-        X=np.array(indicators),
-        fmt="%.5f",
-        header="min_wss max_wss min_wss_aneurysm max_wss_aneurysm min_wss_vessels max_wss_vessels mean_wss std_wss mean_wss_aneurysm std_wss_aneurysm mean_wss_vessels std_wss_vessels mean_osi_aneurysm max_osi_aneurysm KER VDR LSA HSA SCI ICI",
-        comments='')
+                # Compute indicators over a cardiac cycle
+                time_steps = xdmf_file.get_time_steps()
+                for t in time_steps[-T_cardiac_cycle:]:
+                    xdmf_file.update_time_step(t)
+                    indicators.append(compute_indicators(xdmf_file, vessel_in_out_origin, vessel_in_out_plane, orifice_origin, orifice_plane))
+
+                # Compute min, max and mean over the cardiac cycle
+                indicators = np.array(indicators)
+                min = np.min(indicators, axis=0)
+                max = np.max(indicators, axis=0)
+                mean = np.mean(indicators, axis=0)
+                std = np.std(indicators, axis=0)
+                indicators = np.concatenate((indicators, min.reshape((1,-1)), max.reshape((1,-1)), mean.reshape((1,-1)), std.reshape((1,-1))))
+
+                np.savetxt(
+                    fname=osp.join(data_dir, "csv", f"{filename.split('/')[-1][:-5]}.csv"),
+                    X=np.array(indicators),
+                    fmt="%.5f",
+                    header="min_wss max_wss min_wss_aneurysm max_wss_aneurysm min_wss_vessels max_wss_vessels mean_wss std_wss mean_wss_aneurysm std_wss_aneurysm mean_wss_vessels std_wss_vessels mean_osi_aneurysm max_osi_aneurysm KER VDR LSA HSA SCI ICI",
+                    comments='')
+                
+                bar()
+        print("Done.")
