3693e3bbe73325e9080824cddd6ae0a9a4b2d67f
UgoPelissier <upelissier@gmail.com>
2023-10-31 13:56:38 +0000

    🚀

--- AnXplore_analysis_framework.py
+++ AnXplore_analysis_framework.py
@@ -3,14 +3,15 @@ import os.path as osp
 import numpy as np
 from alive_progress import alive_bar
 
-from utils.vtk import decompress_h5
+from utils.xdmf import XDMF_Wrapper
 from utils.indicators import compute_indicators
 
 if __name__ == '__main__':
 
     data_dir = "data"
-    vtu_dir = osp.join(data_dir, "vtu")
     filename = "AnXplore178_FSI.xdmf"
+
+    xdmf_file = XDMF_Wrapper(osp.join(data_dir, filename))    
 
     vessel_in_out_origin = [0.0, 0.001, 0.0]
     vessel_in_out_plane = [0.0, 1.0, 0.0]
@@ -20,14 +21,15 @@ if __name__ == '__main__':
 
     T_cardiac_cycle = 20
 
-    decompress_h5(data_dir, filename)
-
     os.makedirs(osp.join(data_dir, "csv"), exist_ok=True)
     indicators = []
-    cardiac_cycle = sorted(os.listdir(vtu_dir))[-T_cardiac_cycle:]
-    with alive_bar(len(cardiac_cycle), title="Computing indicators over a cardiac cycle...") as bar:
-        for time_step in cardiac_cycle:
-            indicators.append(compute_indicators(vtu_dir, time_step, vessel_in_out_origin, vessel_in_out_plane, orifice_origin, orifice_plane))
+
+    # Compute indicators over a cardiac cycle
+    with alive_bar(T_cardiac_cycle, title="Computing indicators over a cardiac cycle...") as bar:
+        time_steps = xdmf_file.get_time_steps()
+        for t in time_steps[-T_cardiac_cycle:]:
+            xdmf_file.update_time_step(t)
+            indicators.append(compute_indicators(xdmf_file, vessel_in_out_origin, vessel_in_out_plane, orifice_origin, orifice_plane))
             bar()
     print("Done.")
 
@@ -45,5 +47,3 @@ if __name__ == '__main__':
         fmt="%.5f",
         header="min_wss max_wss min_wss_aneurysm max_wss_aneurysm min_wss_vessels max_wss_vessels mean_wss std_wss mean_wss_aneurysm std_wss_aneurysm mean_wss_vessels std_wss_vessels mean_osi_aneurysm max_osi_aneurysm KER VDR LSA HSA SCI ICI",
         comments='')
-    
-    os.system(f"rm -rf {vtu_dir}")
