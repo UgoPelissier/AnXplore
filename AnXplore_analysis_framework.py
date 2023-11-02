191154d72352df20c2a06fa33f321b1574a8365f
UgoPelissier <upelissier@gmail.com>
2023-11-02 09:10:37 +0100

    🚀

--- AnXplore_analysis_framework.py
+++ AnXplore_analysis_framework.py
@@ -10,7 +10,9 @@ from utils.indicators import compute_indicators
 if __name__ == '__main__':
 
     data_dir = "data"
-    cases = ['fsi']
+    cases = ['rigid', 'fsi']
+
+    os.makedirs(osp.join(data_dir, "csv"), exist_ok=True)
 
     vessel_in_out_origin = [0.0, 0.001, 0.0]
     vessel_in_out_plane = [0.0, 1.0, 0.0]
@@ -21,13 +23,12 @@ if __name__ == '__main__':
     T_cardiac_cycle = 20
 
     for case in cases:
+        os.makedirs(osp.join(data_dir, "csv", case), exist_ok=True)
         filenames = glob.glob(osp.join(data_dir, case, "*.xdmf"))
 
         with alive_bar(len(filenames), title=f"Computing indicators for {case} case") as bar:
             for filename in filenames:
                 xdmf_file = XDMF_Wrapper(osp.join(filename))    
-
-                os.makedirs(osp.join(data_dir, "csv"), exist_ok=True)
                 indicators = []
 
                 # Compute indicators over a cardiac cycle
@@ -45,7 +46,7 @@ if __name__ == '__main__':
                 indicators = np.concatenate((indicators, min.reshape((1,-1)), max.reshape((1,-1)), mean.reshape((1,-1)), std.reshape((1,-1))))
 
                 np.savetxt(
-                    fname=osp.join(data_dir, "csv", f"{filename.split('/')[-1][:-5]}.csv"),
+                    fname=osp.join(data_dir, "csv", case, f"{filename.split('/')[-1][:-5]}.csv"),
                     X=np.array(indicators),
                     fmt="%.5f",
                     header="min_wss max_wss min_wss_aneurysm max_wss_aneurysm min_wss_vessels max_wss_vessels mean_wss std_wss mean_wss_aneurysm std_wss_aneurysm mean_wss_vessels std_wss_vessels mean_osi_aneurysm max_osi_aneurysm KER VDR LSA HSA SCI ICI",
